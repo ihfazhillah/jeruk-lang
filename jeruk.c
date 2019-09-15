49a0f9d13c032b7b56b5dbf245f506438c5dd470
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-09-15 16:37:24 +0700

    Add eval

--- jeruk.c
+++ jeruk.c
@@ -42,7 +42,8 @@ void add_history (char * unused) {};
 #include <editline/history.h>
 #endif
 
-
+long eval(mpc_ast_t* t);
+long eval_op(long x, char* op, long y);
 
 int main(int argc, char ** argv){
     mpc_parser_t* Number = mpc_new("number");
@@ -73,7 +74,8 @@ int main(int argc, char ** argv){
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Jeruk, &r)){
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
@@ -85,5 +87,35 @@ int main(int argc, char ** argv){
 
     mpc_cleanup(4, Number, Operator, Expression, Jeruk);
 
+    return 0;
+}
+
+
+long eval(mpc_ast_t* t){
+    if (strstr(t->tag, "number")){
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child. */
+    char* op = t->children[1]->contents;
+
+    /* we store the third child in `x`  */ 
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0)  return x + y; 
+    if (strcmp(op, "-") == 0)  return x - y;
+    if (strcmp(op, "*") == 0)  return x * y;
+    if (strcmp(op, "/") == 0)  return x / y;
     return 0;
 }
