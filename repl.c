32876ef818598a614059663b7155d6c6129ca3fd
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-09-15 07:48:54 +0700

    Add fake readline library for windows

--- repl.c
+++ repl.c
@@ -18,8 +18,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+
+static char buffer[2048];
+
+/*  fake readline function */
+char * readline(char * prompt){
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char * cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = "\0";
+    return cpy;
+}
+
+void add_history (char * unused) {};
+
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
 
 
